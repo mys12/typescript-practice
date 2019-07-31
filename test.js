45c40b7e555ecb9523b3d7867c3ce2a965a9aef7
mys12 <mys62811@naver.com>
2019-07-31 16:17:19 +0900

    practice typescript

---
+++ test.js
@@ -0,0 +1,7 @@
+// undefined, null, string, number, object, function
+var myNumber = 5;
+console.log(myNumber);
+function add(a, b) {
+    return a + b;
+}
+add(5, 10);
---
+++ test.ts
@@ -0,0 +1,9 @@
+// undefined, null, string, number, object, function
+
+const myNumber: number = 5;
+console.log(myNumber);
+
+function add(a: number, b: number) {
+    return a+b;
+}
+add(5,10);
